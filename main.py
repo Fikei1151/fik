1bbffd888edca6bda1bce86507f4e4218b7c6dc7
Fikree Hajiyusof <fikree205m@gmail.com>
2023-12-26 17:29:50 +0700

    Quiz 3

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty, ListProperty
 
 class QuizScreen(BoxLayout):
-    def check_answer(self, instance):
-        result_label = self.ids['result_label']
-        if instance.text == '2':
-            result_label.text = 'Good'
+    question_label = ObjectProperty()
+    result_label = ObjectProperty()
+    answer_buttons = ListProperty()
+    current_question_index = 0
+    questions = ListProperty([
+        {"question": "What is 2 + 2?", "answers": ["3", "4", "5"]},
+        {"question": "What is the capital of France?", "answers": ["Paris", "London", "Berlin"]},
+        {"question": "Who wrote 1984?", "answers": ["Orwell", "Shakespeare", "Tolkien"]}
+    ])
+
+    def on_questions(self, instance, value):
+        if value:
+            self.load_question()
+
+    def load_question(self):
+        # ตรวจสอบว่ายังมีคำถามในลิสต์
+        if self.current_question_index < len(self.questions):
+            self.question_label.text = self.questions[self.current_question_index]['question']
+            self.answer_buttons = self.questions[self.current_question_index]['answers']
         else:
-            result_label.text = 'Bad Try agin'
+            self.answer_buttons = ["", "", ""]
+    def check_answer(self, answer):
+        correct_answer = self.questions[self.current_question_index]['answers'][1]
+        if answer == correct_answer:
+            self.result_label.text = 'Correct!'
+            self.current_question_index += 1
+            if self.current_question_index < len(self.questions):
+                self.load_question()
+            else:
+                self.result_label.text = 'Quiz Completed!'
+        else:
+            self.result_label.text = 'Incorrect! Try again.'
 
 class QuizApp(App):
     def build(self):
-        return QuizScreen()
+        screen = QuizScreen()
+        screen.load_question()  # Load the first question immediately
+        return screen
 
 if __name__ == '__main__':
     QuizApp().run()
